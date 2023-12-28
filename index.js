e764ca122b442a037dcfc6ef607e6186336b9090
KrrishSehgal <krrishsehgal03@gmail.com>
2023-12-28 20:18:06 +0530

    1.0.2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const hostname ='127.0.0.1';
 
 // EXPRESS SPECIFIC STUFF
